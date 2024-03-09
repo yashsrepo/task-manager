8cda5ec1f3189d1a497daf4097abe52f490b7e29
yashsrepo <yashk6767@gmail.com>
2024-03-09 14:24:57 +0530

    added get routes for tasks

--- src/index.js
+++ src/index.js
@@ -38,6 +38,27 @@ app.get('/users/:id', (req, res) => {
         res.status(500).send()
     })
 })
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
